dd9b22128e511216235728cfd8fedde638c1ff84
Valentan390 <137200405+Valentan390@users.noreply.github.com>
2023-11-13 21:14:24 +0200

    Updated Favorits

--- src/components/favoritsCarsCards/FavoritsCarsCards.jsx
+++ src/components/favoritsCarsCards/FavoritsCarsCards.jsx
@@ -1,9 +1,7 @@
 import { useSelector } from 'react-redux';
 import { getFilterFavorite } from 'redux/favorite/selectorsFavorite';
 import styles from './FavoritsCarsCards.module.css';
-
 import CarItem from 'components/carItem/CarItem';
-import CarFilterFavorits from 'components/carFilterFavorits/CarFilterFavorits';
 
 const FavoritsCarsCards = () => {
   const filterFavoritsCars = useSelector(getFilterFavorite);
@@ -11,15 +9,11 @@ const FavoritsCarsCards = () => {
   return (
     <>
       {filterFavoritsCars.length > 0 ? (
-        <>
-          {' '}
-          <CarFilterFavorits />
-          <ul className={styles.advertsList}>
-            {filterFavoritsCars.map(car => {
-              return <CarItem car={car} key={car.id} />;
-            })}
-          </ul>
-        </>
+        <ul className={styles.advertsList}>
+          {filterFavoritsCars.map(car => {
+            return <CarItem car={car} key={car.id} />;
+          })}
+        </ul>
       ) : (
         <div className={styles.noMatching}>
           Oops, there's nothing here. Go to the catalog to add cars to your
--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,8 +1,10 @@
+import CarFilterFavorits from 'components/carFilterFavorits/CarFilterFavorits';
 import FavoritsCarsCards from 'components/favoritsCarsCards/FavoritsCarsCards';
 import React from 'react';
 const Favourites = () => {
   return (
     <>
+      <CarFilterFavorits />
       <FavoritsCarsCards />
     </>
   );
