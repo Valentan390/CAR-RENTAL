54faf34b07e0a59a8ef9bac4d1bfaf0d0f7b1155
Valentan390 <137200405+Valentan390@users.noreply.github.com>
2023-11-13 15:32:38 +0200

    Add lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,22 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './layout/Layout';
-import Home from 'pages/Home';
-import Catalog from 'pages/Catalog';
-import Favourites from 'pages/Favourites';
+import { Suspense, lazy } from 'react';
+
+const Home = lazy(() => import('../pages/Home'));
+const Catalog = lazy(() => import('../pages/Catalog'));
+const Favorites = lazy(() => import('../pages/Favourites'));
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="catalog" element={<Catalog />} />
-        <Route path="favourites" element={<Favourites />} />
-      </Route>
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <Suspense>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="catalog" element={<Catalog />} />
+          <Route path="favourites" element={<Favorites />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Suspense>
   );
 };
